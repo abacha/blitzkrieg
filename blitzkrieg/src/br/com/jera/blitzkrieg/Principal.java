6c612b421650b9a8f63784fdd3b1783158d18f11
marcosbeirigo <marcosbeirigo@gmail.com>
2011-04-06 10:02:33 -0400

    cala boca arrombado

--- blitzkrieg/src/br/com/jera/blitzkrieg/Principal.java
+++ blitzkrieg/src/br/com/jera/blitzkrieg/Principal.java
@@ -7,7 +7,6 @@ import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.AutoParallaxBackground;
-import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.Texture;
@@ -16,10 +15,12 @@ import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
+import android.util.DisplayMetrics;
+
 public class Principal extends BaseGameActivity {
 
-	private static final int CAMERA_WIDTH = 720;
-	private static final int CAMERA_HEIGHT = 480;
+	private int CAMERA_WIDTH;
+	private int CAMERA_HEIGHT;
 	private Texture brickTexture;
 	private TextureRegion brickTextureRegion;
 	
@@ -28,9 +29,13 @@ public class Principal extends BaseGameActivity {
 	private TextureRegion mParallaxLayerBack;
 	private TextureRegion mParallaxLayerMid;
 	private TextureRegion mParallaxLayerFront;
-
+	
 	@Override
 	public Engine onLoadEngine() {
+		final DisplayMetrics displayMetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        CAMERA_WIDTH = displayMetrics.widthPixels;
+        CAMERA_HEIGHT= displayMetrics.heightPixels;
 		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT),
 				camera));
